daaa5715a1d17579b6dcc05c5b3e728ff09d47dc
Vova_does <dereviannykh.v@gmail.com>
2022-04-30 17:56:51 +0300

    оружие

--- models/armament/AnElevenFootPole.py
+++ models/armament/AnElevenFootPole.py
@@ -1,9 +1,9 @@
 import ArmamentBase
 
 
-class AnEleven-footPole(ArmamentBase):
+class AnElevenFootPole(ArmamentBase):
     def __init__(self):
-        super(AnEleven-footPole, self).__init__(1, 'Одиннадцатифутовый шест', 200, 1, 2)
+        super(AnElevenFootPole, self).__init__(1, 'Одиннадцатифутовый шест', 200, 1, 2)
 
     def use_armament(self, req, hero):  # надеваем достпехи/оружие
         # в отдельную переменную доб бонус
--- models/armament/TheTreacherousBastardIsSword.py
+++ models/armament/TheTreacherousBastardIsSword.py
@@ -1,9 +1,9 @@
 import ArmamentBase
 
 
-class TheTreacherousBastard'sSword(ArmamentBase):
+class TheTreacherousBastardIsSword(ArmamentBase):
     def __init__(self):
-        super(TheTreacherousBastard'sSword, self).__init__(2, 'Меч коварного ублюдка', 400, 1, 1)
+        super(TheTreacherousBastardIsSword, self).__init__(2, 'Меч коварного ублюдка', 400, 1, 1)
 
     def use_armament(self, req, hero):  # надеваем достпехи/оружие
         # в отдельную переменную доб бонус
